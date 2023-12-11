f965bde125d2dde13f4f8e873e06f3a8034c5001
jeong202 <144635774+L-jy16@users.noreply.github.com>
2023-12-11 17:34:57 +0900

    2023.12.11

--- client/src/components/user/Join.jsx
+++ client/src/components/user/Join.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 
 import firebase from '../../firebase.js'
-// import axios from 'axios';
-// import { useNavigate } from 'react-router-dom'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom'
 
 
 const Join = () => {
@@ -11,10 +11,12 @@ const Join = () => {
     const [youEmail, setYouEmail] = useState("");
     const [youPass, setYouPass] = useState("");
     const [youPassC, setYouPassC] = useState("");
+    const [flag, setFlag] = useState(false);
 
-    // let navigate = useNavigate();
+    let navigate = useNavigate();
 
     const JoinFunc = async (e) => {
+        setFlag(true);
         e.preventDefault();
 
         if (!(youName && youEmail && youPass && youPassC)) {
@@ -32,24 +34,26 @@ const Join = () => {
             displayName: youName,
         });
 
-        //console.log(createdUser.user)
+        console.log(createdUser.user)
 
         // 개인정보 -> mongodb
-        // let body = {
-        //     email: createdUser.user.multiFactor.user.email,
-        //     displayName: createdUser.user.multiFactor.user.displayName,
-        //     uid: reatedUser.user.multiFactor.user.uid
-        // }
+        let body = {
+            email: createdUser.user.multiFactor.user.email,
+            displayName: createdUser.user.multiFactor.user.displayName,
+            uid: createdUser.user.multiFactor.user.uid
+        }
 
-        // axios.post("/api/user/join", body)
-        //     .then((response) => {
-        //         if (response.data.success) {
-        //             // 회원가입 성공
-        //             navigate("/login")
-        //         } else {
-        //             return alert("회원가입이 실패하였습니다.")
-        //         }
-        //     })
+        axios.post("/api/user/join", body)
+            .then((response) => {
+                setFlag(false);
+                if (response.data.success) {
+                    // 회원가입 성공
+                    alert("회원 가입이 완료되었습니다.")
+                    navigate("/login")
+                } else {
+                    return alert("회원가입이 실패하였습니다.")
+                }
+            })
     }
 
     return (
@@ -98,6 +102,7 @@ const Join = () => {
                                 autoComplete="off"
                                 className="input__style"
                                 required
+                                minLength={8}
                                 value={youPass}
                                 onChange={(e) => setYouPass(e.currentTarget.value)}
                             />
@@ -112,11 +117,12 @@ const Join = () => {
                                 className="input__style"
                                 autoComplete="off"
                                 required
+                                minLength={8}
                                 value={youPassC}
                                 onChange={(e) => setYouPassC(e.currentTarget.value)}
                             />
                         </div>
-                        <button type="submit" className="join__btn mt30" onClick={(e) => JoinFunc(e)}>회원가입</button>
+                        <button disabled={flag} type="submit" className="join__btn mt30" onClick={(e) => JoinFunc(e)}>회원가입</button>
                     </fieldset>
                 </form>
             </div>
--- client/src/components/user/Login.jsx
+++ client/src/components/user/Login.jsx
@@ -1,8 +1,34 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import firebase from "../../firebase.js";
 
 const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [errorMsg, setErrorMsg] = useState("");
+    const navigate = useNavigate();
+
+    const LoginFunc = async (e) => {
+        e.preventDefault();
+        if (!(email && password)) {
+            return alert("모든 값을 채워주세요");
+        }
+        try {
+            await firebase.auth().signInWithEmailAndPassword(email, password);
+            alert("로그인 성공하셨습니다.")
+            navigate("/")
+        }
+        catch (err) {
+            console.log(err)
+            if (err.code === "auth/invalid-email") {
+                setErrorMsg("존재하지 않은 이메일입니다.")
+            } else if (err.code === "auth/invalid-credential") {
+                setErrorMsg("비밀번호가 일치하지 않습니다.")
+            } else {
+                setErrorMsg("로그인 실패하였습니다.")
+            }
+        }
+    }
 
     return (
         <div className='login__wrap'>
@@ -39,7 +65,10 @@ const Login = () => {
                             onChange={(e) => setPassword(e.currentTarget.value)}
                         />
                     </div>
-                    <button type="submit" className="login__btn mt30">로그인</button>
+                    <div>
+                        {errorMsg !== "" && <p>{errorMsg}</p>}
+                    </div>
+                    <button type="submit" onClick={(e) => LoginFunc(e)} className="login__btn mt30">로그인</button>
                 </fieldset>
             </form>
         </div>
